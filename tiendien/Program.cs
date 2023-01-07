dfe73213c4b34e192232ce9a8081ae861669b704
hieuelcom <112467458+hieuelcom@users.noreply.github.com>
2023-01-07 09:48:01 +0700

    kkjh jk

---
+++ tiendien/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+using tiendien;
+public class progam
+{
+    static void Main(string[] args)
+
+    {
+        KhachHangVN a = new KhachHangVN(1, "Nguyen Trung Hieu", "2022-09-22", 219, "Cá nhân");
+        KhachHangNN b = new KhachHangNN(2, "Lionel Messi", "2022-09-21", 156, "US");
+        Console.WriteLine(a.ThanhTien);
+        Console.WriteLine(b.ThanhTienNN);
+        
+    }
+}
